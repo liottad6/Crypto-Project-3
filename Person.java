c341d1acb50592f38d84e192f6cf240faaf449b7
David Liotta <liottad6@students.rowan.edu>
2018-12-10 18:24:20 -0500

    Filled in encrypt and decrypt

--- Person.java
+++ Person.java
@@ -1,8 +1,9 @@
-package proj3;
+package Crypto;
+import java.util.Random;
 
 /**
  * @author David Liotta
- *
+ * The person class to be used in conjunction with RSA
  */
 
 public class Person {
@@ -10,19 +11,20 @@ public class Person {
 	long m;			//public modulus
 	long d;			//private exponent
 
+	/**
+	 * Constructor for Person
+	 * Sets up the variables needed for RSA
+	 */
 	public Person() {
-		long p = randPrime(from, to, rand);
-		long q = randPrime(from, to, rand);
+		Random rand = new Random();
+		long p = RSA.randPrime(1, 500, rand);
+		long q = RSA.randPrime(1, 500, rand);
+		long n = (q - 1) * (p - 1);
 		this.m = q * p;
-		this.e = randPrime(from, to, rand);  //TODO Make this relatively prime to m
-		this.d = inverse(e, m);
-		
+		this.e = RSA.relPrime(m, rand); 
+		this.d = RSA.inverse(e, m);
 	}
 	
-	/**
-	 *  Accessor method for public modulus
-	 * @return m
-	 */
 	public long getM() {
 		return this.m;
 	}
@@ -31,13 +33,39 @@ public class Person {
 		return this.e;
 	}
 	
-	public long[] encryptTo(String msg, Person she) {
-		return null;
-		
+	/**
+	 * Method that encrypts a message using RSA encryption
+	 * @param msg the message to decrypt
+	 * @param she the person who we are sending the message to
+	 * @return the encrypted message
+	 */
+	public long[] encryptTo(String msg, Person she) {	
+		long[] encrBytes = new long[msg.length() / 2];
+	
+		for(int i = 0; i < msg.length(); i += 2) {
+			long tempMsg = RSA.toLong(msg, i);				//Retrieving each 2 chars converted to a long
+			tempMsg = RSA.modPower(tempMsg, she.getE(), she.getM());	//Applying the exponent and mod
+			encrBytes[i / 2] = tempMsg;					//Building the array
+		}
+		return encrBytes;
 	}
 	
+	/**
+	 * Method that decrypts an array of longs to a readable string
+	 * @param cipher the array of encrypted bytes
+	 * @return the decrypted message
+	 */
 	public String decrypt(long[] cipher) {
-		return null;
+		String decrypted = "";
+		
+		for(int i = 0; i < cipher.length; i++) {
+			long tempBit = cipher[i];			//Retrieve the bit to decrypt
+			tempBit = RSA.modPower(tempBit, e, m);		//Decrypt it
+			String temp = RSA.longTo2Chars(tempBit);	//Convert to a string
+			decrypted += temp;
+		}
+		return decrypted;
 	}
 
 }
+
